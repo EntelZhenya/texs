9ec4dad4b319503bde6fb3d94edc95818820448b
Jenya <entel1982@gmail.com>
2020-03-18 18:08:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var leftBorderWidht = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(leftBorderWidht);
+
+// let answer = confirm("Есть ли 18");
+
+// console.log(answer);
+
+let answer = prompt("Есть ли 18", "Yes");
+
+console.log(typeof (answer));
